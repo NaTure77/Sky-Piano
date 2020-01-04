33869fab36aa85089154837ff4de5f967f02a47b
NaTure77 <xnaudrornfl@gmail.com>
2020-01-04 20:38:51 +0900

    First Commit

---
+++ Assets/Scripts/PressKey.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressKey : MonoBehaviour
+{
+    public AudioClip source;
+    public void Awake()
+    {
+        source = (AudioClip)Resources.Load(gameObject.name);
+    }
+}
